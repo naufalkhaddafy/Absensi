aae520a27e32a2a6997e245c7fa53d26fca6a784
naufalkhaddafy <naufalkhaddafy@webmail.umm.ac.id>
2022-06-21 19:20:08 +0700

    Repair Sistem

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,6 +14,13 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        \App\Models\User::create([
+            'name' => 'admin',
+            'email' => 'admin@gmail.com',
+            'password' => Hash::make('password'),
+            'level' => 'admin',
+            //'divisi' => $data['divisi'],
+            //'nohp' => $data['nohp'],
+        ]);
     }
 }
